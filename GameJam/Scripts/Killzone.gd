1c9c631bcab5c91283cc5075d015f71b8b1d6fff
valentecaio <cvalente.aio@gmail.com>
2024-08-17 12:54:07 -0300

    wireframe walls

--- GameJam/Scripts/Killzone.gd
+++ GameJam/Scripts/Killzone.gd
@@ -1,11 +1,11 @@
 extends Area3D
 
 func die():
-  print("Died!")
-  get_tree().reload_current_scene()
+    print("Died!")
+    get_tree().reload_current_scene()
 
 func _on_body_entered(body):
-  die()
+    die()
 
 func _on_area_entered(area):
-  die()
+    die()
