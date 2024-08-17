e5ef420274f3f6534920ec046bf558e9faea8215
Gabrielle Brandemburg dos Anjos <gabriellebrand@tecgraf.puc-rio.br>
2024-08-17 12:29:33 -0300

    change ident space

--- GameJam/Scripts/Main.gd
+++ GameJam/Scripts/Main.gd
@@ -3,8 +3,8 @@ extends Node3D
 const wall_scene = preload("res://Scenes/Wall.tscn")
 
 func _process(delta):
-  pass
+    pass
 
 func _on_timer_timeout():
-  var wall = wall_scene.instantiate()
-  add_child(wall)
+    var wall = wall_scene.instantiate()
+    add_child(wall)
