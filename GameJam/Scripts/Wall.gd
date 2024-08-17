bf54b1b837613ef795ed655b121e42585fe4ea5e
valentecaio <cvalente.aio@gmail.com>
2024-08-17 20:55:46 -0300

    move wall holes

--- GameJam/Scripts/Wall.gd
+++ GameJam/Scripts/Wall.gd
@@ -1,6 +1,6 @@
 extends Node3D
 
-@export var speed = 2.5
+@export var speed = 3.5
 
 @onready var wall_part_mesh_top = $WallPartMeshTop
 @onready var wall_part_mesh_left = $WallPartMeshLeft
@@ -11,16 +11,17 @@ const wh = 6.0
 const ww = 10.0
 
 # min and max z depth for walls
-# wall starts at z=z_initial and moves up to a positive z=z_final
+# wall spawns at z=z_initial and moves up to a positive z=z_final
 const z_initial = -10
 const z_final = 3
 
-# rolw min/max dimensions
+# hole min/max dimensions
 const hole_h_min = 0.5
 const hole_h_max = 4.0
-const hole_area = 2.0
+const hole_area = 4.0
 
 func _ready():
+    # wall spawn at z_initial
     position.z = z_initial
 
     # hole dimensions
@@ -30,21 +31,24 @@ func _ready():
     # dimension x of left/right wall parts
     var bx = (ww-w)/2
 
+    # random hole origin
+    var orig = randf_range(-0.9*bx, 0.9*bx)
+
     # distance to center of wall parts
     var dx = (ww-bx)/2
     var dy = wh-((wh-h)/2)
 
     # top
-    wall_part_mesh_top.position = Vector3(0, wh-((wh-h)/2.0), 0)
+    wall_part_mesh_top.position = Vector3(orig, wh-((wh-h)/2.0), 0)
     wall_part_mesh_top.scale = Vector3(w, wh-h, 1)
 
     # left
-    wall_part_mesh_left.position = Vector3(-dx, wh/2, 0)
-    wall_part_mesh_left.scale = Vector3(bx, wh, 1)
+    wall_part_mesh_left.position = Vector3(-dx + orig/2, wh/2, 0)
+    wall_part_mesh_left.scale = Vector3(bx+orig, wh, 1)
 
-    # left
-    wall_part_mesh_right.position = Vector3(dx, wh/2, 0)
-    wall_part_mesh_right.scale = Vector3(bx, wh, 1)
+    # right
+    wall_part_mesh_right.position = Vector3(dx + orig/2, wh/2, 0)
+    wall_part_mesh_right.scale = Vector3(bx-orig, wh, 1)
 
 
 func _process(delta):
