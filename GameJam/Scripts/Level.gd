3d712167cdb7e157131af767e9f8eb708bc7f320
valentecaio <cvalente.aio@gmail.com>
2024-08-18 19:03:39 -0300

    add vinyl background

--- GameJam/Scripts/Level.gd
+++ GameJam/Scripts/Level.gd
@@ -2,12 +2,15 @@ extends Node3D
 
 const wall_scene = preload("res://Scenes/Wall.tscn")
 
+@onready var background_vinyl = $BackgroundVinyl
+
 var current_run_time = 0;
 signal current_run_time_changed(run_time)
 
 func _process(delta):
     current_run_time += delta
     emit_signal("current_run_time_changed", current_run_time)
+    background_vinyl.rotate(Vector3(0,0,1), delta)
 
 func _on_timer_timeout():
     var wall = wall_scene.instantiate()
