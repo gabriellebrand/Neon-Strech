2e75afd96f5ed5388d2ae7d6ebafdfead0f209d5
valentecaio <cvalente.aio@gmail.com>
2024-08-18 18:11:41 -0300

    press space/enter to start game

--- GameJam/Scripts/UI/main_menu.gd
+++ GameJam/Scripts/UI/main_menu.gd
@@ -2,13 +2,14 @@ extends Control
 
 @onready var high_score_label: Label = $VBoxContainer/HighScoreLabel
 
-func _ready() -> void:
+func _ready():
     update_high_score_label(0)
     
 func _process(delta):
-    pass
+    if Input.is_action_just_pressed("menu_confirm"):
+        _on_play_button_pressed()
 
-func _on_play_button_pressed() -> void:
+func _on_play_button_pressed():
     GameManager.start_run()
     
 func update_high_score_label(score):
