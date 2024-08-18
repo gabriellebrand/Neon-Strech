97c84904f7b7d1df14c3d111d0839c7cf1fa54a1
valentecaio <cvalente.aio@gmail.com>
2024-08-18 18:34:02 -0300

    better controls

--- GameJam/Scripts/Player.gd
+++ GameJam/Scripts/Player.gd
@@ -6,9 +6,9 @@ extends CharacterBody3D
 signal state_changed(new_value)
 
 const MOVE_SPEED = 4.0
-const STRETCH_SPEED = 0.07
-const MINIMUM_STRETCH_Y = 0.2
-const MAXIMUM_STRETCH_Y = 3.0
+const STRETCH_BASE_SPEED = 0.07
+const STRETCH_Y_MIN = 0.2
+const STRETCH_Y_MAX = 3.0
 
 func _ready():
     snap_to_ground()
@@ -28,8 +28,10 @@ func move(direction):
     velocity.x = (direction * MOVE_SPEED) if direction else move_toward(velocity.x, 0, MOVE_SPEED)
 
 func stretch_y(value):
-    var new_scale_y = scale.y + value * STRETCH_SPEED
-    new_scale_y = clamp(new_scale_y, MINIMUM_STRETCH_Y, MAXIMUM_STRETCH_Y)
+    var normalized_scale = (scale.y - STRETCH_Y_MIN) / (STRETCH_Y_MAX - STRETCH_Y_MIN)
+    var stretch_factor = 1.05 - pow((normalized_scale - 0.5), 2) * 4.0
+    var new_scale_y = scale.y + value * STRETCH_BASE_SPEED * stretch_factor
+    new_scale_y = clamp(new_scale_y, STRETCH_Y_MIN, STRETCH_Y_MAX)
     scale.x = 1 / new_scale_y
     scale.y = new_scale_y
 
